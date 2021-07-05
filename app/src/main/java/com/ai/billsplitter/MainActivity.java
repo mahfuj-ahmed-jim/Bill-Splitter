edba3ec6c46ed078e70732507a8af9cf945ae49d
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2021-07-05 06:10:32 +0600

    Dark mode added

--- app/src/main/java/com/ai/billsplitter/MainActivity.java
+++ app/src/main/java/com/ai/billsplitter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ai.billsplitter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // night mode
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+
+        }
+
     }
 }
